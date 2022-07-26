ff27e9a21dbb0fd4ed792124e8497bb0804d034c
nutahiroba <74714289+nutahiroba@users.noreply.github.com>
2022-07-26 10:10:04 +0900

    【100】ファイル分割エラーの修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import './components/store.tsx'
+import Store from './components/store'
 function App() {
 
   return (
     <div className="App">
       <h1>メシレコ</h1>
-      <store></store>
+      <Store></Store>
     </div>
   )
 }
