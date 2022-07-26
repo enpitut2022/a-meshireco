4d31a44418af5d1f14695425c629d800356ff78b
nutahiroba <74714289+nutahiroba@users.noreply.github.com>
2022-07-26 10:29:18 +0900

    【100】インデント修正

--- src/components/store.tsx
+++ src/components/store.tsx
@@ -3,9 +3,9 @@ const store = () => {
 
   return(
     <div>
-    <h2>粉とクリーム</h2>
-    <p>10:00~17:00</p>
-    <img src="https://kosei.sec.tsukuba.ac.jp/wp-content/uploads/P1050398.jpg" />
+      <h2>粉とクリーム</h2>
+      <p>10:00~17:00</p>
+      <img src="https://kosei.sec.tsukuba.ac.jp/wp-content/uploads/P1050398.jpg" />
     </div>
   )
 };
