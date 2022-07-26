72be5a0154ba3b7059902c9a324b8c12404e34d5
nutahiroba <74714289+nutahiroba@users.noreply.github.com>
2022-07-26 10:01:38 +0900

    【100】ファイル分割

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
+import './components/store.tsx'
 function App() {
 
   return (
     <div className="App">
       <h1>メシレコ</h1>
-      <div>
-      <h2>粉とクリーム</h2>
-      <p>10:00~17:00</p>
-      <img src="https://kosei.sec.tsukuba.ac.jp/wp-content/uploads/P1050398.jpg" />
-      </div>
+      <store></store>
     </div>
   )
 }
--- src/components/store.tsx
+++ src/components/store.tsx
@@ -1,3 +1,13 @@
-const store = () => {};
+//@ts-ignore
+const store = () => {
+
+  return(
+    <div>
+    <h2>粉とクリーム</h2>
+    <p>10:00~17:00</p>
+    <img src="https://kosei.sec.tsukuba.ac.jp/wp-content/uploads/P1050398.jpg" />
+    </div>
+  )
+};
 
 export default store;
