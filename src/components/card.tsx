c3d8186b7fc7a715e2c8522e10b468a787ed8bed
Taichi Nakashima <1111yusuke@gmail.com>
2022-07-26 16:18:27 +0900

    【202_204】useMemoして引き継ぐ

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,5 +1,8 @@
+import React, { useRef, useMemo } from 'react'
 import TinderCard from 'react-tinder-card'
 import Store from './store'
+import './card.css'
+import store from './store';
 
 // ...
 interface store {
@@ -8,40 +11,56 @@ interface store {
 }
 
 const card = () => {
-    const storeList: Array<store> = [
-      {name: '粉とクリーム', text: 'aaa'},
-      {name: 'むじゃき', text: 'bbb'},
-    ]
 
-    //@ts-ignore
-    const onSwipe = (direction) => {
-      console.log('You swiped: ' + direction)
-    }
-    
-    //@ts-ignore
-    const onCardLeftScreen = (myIdentifier) => {
-      console.log(myIdentifier + ' left the screen')
-    }
+  const storeList: Array<store> = [
+    { name: '粉とクリーム', text: 'aaa' },
+    { name: 'むじゃき', text: 'bbb' },
+  ]
 
-    return (
+  const childRef = useMemo<any>(
+    () =>
+      Array(storeList.length).fill(0).map((i) => React.createRef()),
+    [storeList.length]
+  )
+
+
+  //@ts-ignore
+  const onSwipe = (direction) => {
+    console.log('You swiped: ' + direction)
+  }
+
+  //@ts-ignore
+  const onCardLeftScreen = (myIdentifier) => {
+    console.log(myIdentifier + ' left the screen')
+  }
+
+  return (
+    //@ts-ignore
+    <div class="st">
+      {storeList.map((store, index) => (
         //@ts-ignore
-        storeList.map((store) => (
-          //@ts-ignore
-          <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>  
-            <Store
-              name={store.name}
-              text={store.text}
-            ></Store>
-          </TinderCard>
-        ))
+        <TinderCard
+          class="tinderCard"
+          ref={childRef[index]}
+          key={index}
+          onSwipe={onSwipe}
+          onCardLeftScreen={() => onCardLeftScreen('fooBar')}
+          preventSwipe={['right', 'left']}>
+          <Store
+            name={store.name}
+            text={store.text}
+          ></Store>
+        </TinderCard>
+      ))}
+    </div>
 
-        // <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>
-        //     <Store
-        //       name={storeList[0].name}
-        //       text={storeList[0].text} 
-        //     ></Store>
-        // </TinderCard>
-    )
+    // <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>
+    //     <Store
+    //       name={storeList[0].name}
+    //       text={storeList[0].text} 
+    //     ></Store>
+    // </TinderCard>
+  )
 }
 
 export default card;
