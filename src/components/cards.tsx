e4c49ecbec592c5b96d13ac2365c6307bfa45d05
some0419 <61790336+some0419@users.noreply.github.com>
2022-07-27 16:13:24 +0900

    【303】現在地と飲食店間の距離計算の作成

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -3,17 +3,18 @@ import TinderCard from 'react-tinder-card'
 import Store from './store'
 import '../index.css'
 import './tmpStoreList'
-import {tmpStoreList, store} from './tmpStoreList'
+import { tmpStoreList, store } from './tmpStoreList'
+import { useState, useEffect, useRef } from 'react'
 
-const storeList: Array<store> = tmpStoreList 
+const storeList: Array<store> = tmpStoreList
 
 // 受け取ったリストをシャッフルする
 const shuffleArray = ([...array]) => {
-	for (let i = array.length - 1; i >= 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[array[i], array[j]] = [array[j], array[i]];
-	}
-	return array;
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 const storeListShaffled: Array<store> = shuffleArray(storeList)
 
@@ -38,30 +39,66 @@ const cards = () => {
     console.log(myIdentifier + ' left the screen')
   }
 
+  
+  const [isAvailable, setAvailable] = useState(false);
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+
+  const isFirstRef = useRef(true);
+
+  useEffect(() => {
+    isFirstRef.current = false;
+    getCurrentPosition();
+    if ('geolocation' in navigator) {
+      setAvailable(true);
+    }
+  }, [isAvailable]);
+
+  const getCurrentPosition = () => {
+    console.log('位置情報を取得しました');
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      setPosition({ latitude, longitude });
+    });
+  };
+
+  const R = Math.PI / 180;
+
+  const distance = (lat1: number, lng1: number, lat2: number, lng2: number) => {
+    lat1 = R;
+    lng1 = R;
+    lat2 = R;
+    lng2 = R;
+    return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
+  }
+
   return (
     <div className='cards'>
+      
       {storeListRiverse.map((store, index) => (
+        
+          //distance({position.latitude}, {position.longitude}, {store.latitude}, {store.longitude})
+        
         // @ts-ignore
         <TinderCard
           className='card'
           // TODO リストのリバースをせずcssで実装したい
           // style = {{zIndex: - index}}
-          ref = {childRef[index]}
-          key = {index}
-          onSwipe = {onSwipe}
-          onCardLeftScreen = {() => onCardLeftScreen('fooBar')}
-          preventSwipe = {['right', 'left', 'down']}>
+          ref={childRef[index]}
+          key={index}
+          onSwipe={onSwipe}
+          onCardLeftScreen={() => onCardLeftScreen('fooBar')}
+          preventSwipe={['right', 'left', 'down']}>
           <Store
-            name = {store.name}
-            openTime = {store.openTime}
-            closeTime = {store.closeTime}
-            price = {store.price}
-            map = {store.map}
-            tabeLog = {store.tabeLog}
-            image = {store.image}
-            category = {store.category}
-            latitude = {store.latitude || 0}
-            longitude = {store.longitude || 0}
+            name={store.name}
+            openTime={store.openTime}
+            closeTime={store.closeTime}
+            price={store.price}
+            map={store.map}
+            tabeLog={store.tabeLog}
+            image={store.image}
+            category={store.category}
+            latitude={store.latitude || 0}
+            longitude={store.longitude || 0}
           ></Store>
         </TinderCard>
       ))}
