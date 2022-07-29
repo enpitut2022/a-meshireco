47d64f1ffeb55662eb6a27d9b12948097e59eaca
some0419 <61790336+some0419@users.noreply.github.com>
2022-07-29 12:00:21 +0900

    【805】ボタンのアイコン化

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,8 @@
 import Button from '@mui/material/Button'
 import {GetStoreMapData, GetStoreDetailData} from './cards'
+import IconButton from '@mui/material/IconButton';
+import PlaceIcon from '@mui/icons-material/Place';
+import LocalDiningIcon from '@mui/icons-material/LocalDining';
 
 // @ts-ignore
 const header = () => {
@@ -7,20 +10,22 @@ const header = () => {
     <div className = 'header'>
       <h1>メシレコ</h1>
       <p>上へスワイプして次の店へ</p>
-      <Button
+      <IconButton
+        color = "warning"
         onClick={() => {
           window.open(GetStoreMapData(), '_blank');
         }}
       >
-        GoogleMap
-      </Button>
-      <Button
+        <PlaceIcon />
+      </IconButton>
+      <IconButton
+        color = "warning"
         onClick={() => {
           window.open(GetStoreDetailData(), '_blank');
         }}
       >
-        食べログ
-      </Button>
+        <LocalDiningIcon/>
+      </IconButton>
     </div>
   )
 };
