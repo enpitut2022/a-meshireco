24c819f9f7392734d42bd74253df89823c9d205e
monokemonoke <kawabataryuhei@gmail.com>
2023-03-23 18:56:03 +0900

    【1001】カードの大きさをスマホ向けに調整した

--- src/components/store.tsx
+++ src/components/store.tsx
@@ -30,9 +30,11 @@ const priceRangeList: { [name: string]: string } = {
 const store = (props: StoreProps) => {
   return (
     <Card sx={{
-      width: 600,
-      "@media screen and (max-width:600px)": {
-        width: 200
+      width: 390,
+      height: 750,
+      "@media screen and (max-width:400px)": {
+        width: 350,
+        height: 500,
       },
     }}>
       <CardActionArea>
@@ -40,16 +42,24 @@ const store = (props: StoreProps) => {
           component="img"
           image={props.image}
           alt=" "
+          sx={{
+            height: 390,
+            width: 390,
+            "@media screen and (max-width:400px)": {
+              width: 350,
+              height: 200,
+            },
+          }}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {props.name}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            <p>営業時間: {props.open}</p>
-            <p>定休日: {props.close}</p>
-            <p>価格帯：{priceRangeList[props.price]}円</p>
-            <p>ジャンル：{props.category}</p>
+            <span>営業時間: {props.open}</span>
+            <span>定休日: {props.close}</span>
+            <span>価格帯：{priceRangeList[props.price]}円</span>
+            <span>ジャンル：{props.category}</span>
           </Typography>
         </CardContent>
       </CardActionArea>
